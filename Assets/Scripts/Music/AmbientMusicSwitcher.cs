c180e03c54cfe15078ffb996599d73f9d5234abd
Ilya Tretyakov <kshaddix1413@gmail.com>
2026-05-03 15:44:50 +0300

    Поправил комнату 1

--- Assets/Scripts/Music/AmbientMusicSwitcher.cs
+++ Assets/Scripts/Music/AmbientMusicSwitcher.cs
@@ -1,10 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(AudioSource))]
 public class AmbientMusicSwitcher : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private string triggerTag = "Player";
+
+    private static AudioSource currentAmbientSource;
+    private AudioSource musicSource;
+
+    private void Awake()
     {
-        
+        musicSource = GetComponent<AudioSource>();
+
+        var areaCollider = GetComponent<Collider>();
+        areaCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!string.IsNullOrEmpty(triggerTag) && !other.CompareTag(triggerTag))
+        {
+            return;
+        }
+
+        if (currentAmbientSource == musicSource)
+        {
+            return;
+        }
+
+        if (currentAmbientSource != null && currentAmbientSource.isPlaying)
+        {
+            currentAmbientSource.Stop();
+        }
+
+        if (!musicSource.isPlaying)
+        {
+            musicSource.Play();
+        }
+
+        currentAmbientSource = musicSource;
     }
 }
